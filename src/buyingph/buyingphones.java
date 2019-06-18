7c9fad409b4ea6731c49da213f38a1884cfd0b7f
753813 <753813@123-PC09.student.Tor.Loc>
2019-06-18 16:02:56 -0400

    calc prices

---
+++ src/buyingph/buyingphones.java
@@ -0,0 +1,13 @@
+package buyingph;
+import java.util.Scanner;
+public class buyingphones {
+	public static void main(String[] args) {
+		System.out.println("Enter the regular price of a phone: ");
+		Scanner s = new Scanner(System.in);
+		double price = s.nextDouble();
+		double p1 =price;
+		double p2 = price;
+		double p3 = price/2.0;
+		
+		
+}}
